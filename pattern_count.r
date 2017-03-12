c838fb09f062663330a08d88214f6d2c048872f7
Dennis Stumm <dstumm@yandex.ru>
2017-03-12 13:23:49 +0100

    Add pattern count function

---
+++ pattern_count.r
@@ -0,0 +1,16 @@
+pattern_count <- function(text, pattern) {
+  count <- 0
+  pattern_length <- nchar(pattern)
+  for (i in 0:(nchar(text) - pattern_length)) {
+    if (substr(rep(text), i + 1, i + pattern_length) == pattern)
+      count <- count + 1
+  }
+  return(count)
+}
+
+message("Text")
+text <- scan("stdin", what="character", nlines=1)
+message("Pattern")
+pattern <- scan("stdin", what="character", nlines=1)
+
+print(pattern_count(text, pattern))
